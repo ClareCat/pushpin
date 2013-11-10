4475483bad02854a4fb72e3e1c11208bb9e49b87
Clare Curtis <cfcurti2@illinois.edu>
2013-11-09 21:20:03 -0600

    added Marker objects aaaaand yeah database is working

---
+++ run.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.heroku import Heroku
+app = Flask(__name__)
+app.debug = True
+heroku = Heroku(app)
+db = SQLAlchemy(app)
+db.init_app(app)
