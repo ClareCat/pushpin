4c3112cfeb5159f4a517963a37d400b14ef523f4
Clare Curtis <cfcurti2@illinois.edu>
2013-12-05 17:35:51 -0600

    Added heroku support back

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from flask.ext.heroku import Heroku
 app = Flask(__name__)
 app.secret_key = 'a'
 app.debug = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/pushpin'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/pushpin'
 heroku = Heroku(app)
 db = SQLAlchemy(app)
 db.init_app(app)
